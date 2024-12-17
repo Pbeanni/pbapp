71433a6f760fec14bada47ebb5426e3f456ae820
Pbeanni <pedroallanbfl@gmail.com>
2024-12-17 14:19:59 -0300

    init

---
+++ src/main/java/pbeanni/pbapp/PbappApplication.java
@@ -0,0 +1,13 @@
+package pbeanni.pbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pbeanni/pbapp/PbappApplicationTests.java
@@ -0,0 +1,13 @@
+package pbeanni.pbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
